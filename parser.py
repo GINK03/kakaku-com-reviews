1feb665029ce153972ceed242aeb200df381b1ff
gimpei <gim.kobayashi@gmail.com>
2017-12-05 21:16:39 +0900

    a

---
+++ parser.py
@@ -0,0 +1,55 @@
+import os
+
+import json
+
+import pickle
+
+import gzip
+
+import glob
+
+import re
+
+import bs4
+
+import concurrent.futures
+
+
+def _map(arr):
+  key, names = arr
+
+  type_boxs = []
+  for name in names:
+    soup = bs4.BeautifulSoup( open(name).read() )
+    #print( soup.text.replace('\n', '') ) 
+    boxs = []
+    for box in soup.find_all('div', {'class':'boxIn clearfix minH'}):
+      boxs.append( re.sub(r'\s{1,}', ' ', box.text ) )
+    if boxs == []:
+      continue
+    srcs = [ x['src'] for x in filter(lambda x:x is not None and 'bbs' in x['src'], [ title.find('img') for title in soup.find_all('div', {'class':'title'}) ] ) ]
+    #print( boxs )
+    #print(srcs)
+
+    if len(boxs) != len(srcs):
+      continue
+
+    for src, box in zip(srcs,boxs):
+      types = src[-5:]
+      print(key, types, src,box)
+      type_boxs.append( {'types':types, 'box':box} )
+  open('parsed/{}.json'.format(key), 'w').write( json.dumps(type_boxs, indent=2, ensure_ascii=False) )
+arrs = {}
+for index, name in enumerate(glob.glob('../21/*')):
+  if 'bbs.kakaku.com' not in name:
+    continue
+  print(name)
+  key = index%100
+  if arrs.get(key) is None:
+    arrs[key] = []
+  arrs[key].append(name)
+arrs = [(key,names) for key, names in arrs.items() ]
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=8) as exe:
+  exe.map(_map, arrs)
+
---
+++ prepare.py
@@ -0,0 +1,56 @@
+import sys
+
+import json
+
+import pickle
+
+import gzip
+
+import glob
+
+from collections import Counter
+
+import math
+
+import MeCab 
+if '--wakati1' in sys.argv:
+  m = MeCab.Tagger('-Owakati')
+
+  data = []
+  for name in glob.glob('parsed/*.json'):
+    print(name)
+    objs = json.loads( open(name).read() )
+    for obj in objs:
+      types = obj['types'] 
+      box = m.parse(obj['box']).strip().split()
+      term_freq = dict(Counter(box))
+      data.append( (types, term_freq) )
+
+  open('wakati1.json', 'w').write( json.dumps(data, indent=2, ensure_ascii=False) )
+
+if '--term_index1' in sys.argv:
+  wakatis = json.loads( open('wakati1.json').read() ) 
+  terms = set()
+  for types, term_freq in wakatis:
+    [ terms.add(term) for term in list(term_freq.keys()) ]
+
+  term_index = {}
+
+  for index, term in enumerate(terms):
+    term_index[term] = index
+  
+  open('term_index.json', 'w').write( json.dumps(term_index,indent=2,ensure_ascii=False) )
+
+if '--vectorize1' in sys.argv:
+  wakatis = json.loads( open('wakati1.json').read() ) 
+  term_index = json.loads( open('term_index.json').read() )
+
+  f = open('data.txt', 'w')
+  for types, term_freq in wakatis:
+    if types in ['1.gif', '2.gif', '5.gif'] :
+      flags = 1.0
+    else:
+      flags = 0.0
+    vec = ' '.join( ['{}:{}'.format(term_index[term], math.log(freq+1.0)) for term, freq in sorted(term_freq.items(), key=lambda x:term_index[x[0]]) if term_index[term] != 0 ] )
+    data = '{} {}'.format(flags, vec)
+    f.write( data + '\n' )
